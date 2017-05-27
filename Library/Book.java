f5f59d5379131aed7026d96a897fda8fefff1d3c
0S7A <0S7A@DESKTOP-Q1430TQ>
2017-05-27 20:10:12 +0545

    toString method added

--- Library/Book.java
+++ Library/Book.java
@@ -4,32 +4,38 @@ public class Book {
     String publisher;
     int year;
 
+// main function already exists in "Main" class, main function shouldnt be declared more than once
+/*
     public static void main(String[] args) {
         Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
 
-        System.out.println(cheese.title());
-        System.out.println(cheese.publisher());
-        System.out.println(cheese.year());
+        System.out.println(cheese.getTitle());
+        System.out.println(cheese.getPublisher());
+        System.out.println(cheese.getYear());
 
         System.out.println(cheese);
     }
-
+*/
     public Book(String title, String publisher, int year) {
         this.title = title;
         this.publisher = publisher;
         this.year = year;
     }
 
-    public String title() {
+    public String getTitle() {
         return title;
     }
 
-    public String publisher() {
+    public String getPublisher() {
         return publisher;
     }
 
-    public int year() {
+    public int getYear() {
         return year;
+    }
+    
+    public String toString(){
+    	return "Title: " +title+ " \t Publisher: "+ publisher + " \t year: "+year;
     }
 
 }
